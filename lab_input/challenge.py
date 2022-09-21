1d952246443da6f77ec04f90475015e5e82ae59b
YellowChai <YellowChai@users.noreply.github.com>
2022-09-21 16:43:01 +0000

    join and split

--- lab_input/challenge.py
+++ lab_input/challenge.py
@@ -1,18 +1,21 @@
 #!/usr/bin/env python3
 
 def main():
-
+    
+    '''  importing date to get the today's date'''
     from datetime import date
 
+    ''' Getting name input'''
     name = input('please, type your name: ')
-
+    
+    ''' Getting today's day of a week ''' 
     week = ['Monday', 'Tuesday', 'Wednesday', 'Thrusday', 'Friday', 'Saturday', 'Sunday']
     today = week[date.today().weekday()]
 
-
+    ''' Print out ''' 
     print(f"Hello, {name}! Happy {today}!")
 
 
 if __name__ == "__main__":
-
+    
     main()
