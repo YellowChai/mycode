a7c8d048ca3254207c2ea20b4aea644d3a84e255
YellowChai <YellowChai@users.noreply.github.com>
2022-09-22 16:42:55 +0000

    looping techniques with writing and reading into text file

---
+++ introfor/challenge.py
@@ -0,0 +1,12 @@
+
+#!/usr/bin/python3
+
+farms = [{"name": "NE Farm", "agriculture": ["sheep", "cows", "pigs", "chickens", "llamas", "cats"]},
+         {"name": "W Farm", "agriculture": ["pigs", "chickens", "llamas"]},
+         {"name": "SE Farm", "agriculture": ["chickens", "carrots", "celery"]}]
+
+
+
+farm = farms.keys[farms.values().index('pigs')]
+
+print(farm)
