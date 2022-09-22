db8dcf28c9c53e353d5feed41f3fdde3ef215e41
YellowChai <YellowChai@users.noreply.github.com>
2022-09-22 15:00:20 +0000

    Learning to troubleshoot

--- montypython/monty_python3.py
+++ montypython/monty_python3.py
@@ -5,7 +5,7 @@
 round = 0
 answer = " "
 
-while round < 3 and answer != "brian" and answer != "shrubbery":
+while round < 3 and (answer != "brian" and answer != "shrubbery"):
     round += 1     # increase the round counter by 1
     answer = input('Finish the movie title, "Monty Python\'s The Life of ______": ').lower()
     
